51c3eff51f0f10b3d72e4fc89ce69e7f282d6278
Yeongsang Jo <61370901+PapimonLikelion@users.noreply.github.com>
2021-12-06 17:26:16 +0900

    feat: 백엔드 초기 설정

---
+++ backend/src/main/java/com/newsletter/cstoday/CsTodayApplication.java
@@ -0,0 +1,12 @@
+package com.newsletter.cstoday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsTodayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CsTodayApplication.class, args);
+    }
+}
---
+++ backend/src/test/java/com/newsletter/cstoday/CsTodayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.newsletter.cstoday;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsTodayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
