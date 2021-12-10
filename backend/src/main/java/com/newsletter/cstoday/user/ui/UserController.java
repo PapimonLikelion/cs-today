a5ed720d39a9596eb8bd9c5100eb694c744a31bd
Yeongsang Jo <61370901+PapimonLikelion@users.noreply.github.com>
2021-12-10 11:14:48 +0900

    feat: cors 매핑 추가

--- backend/src/main/java/com/newsletter/cstoday/user/ui/UserController.java
+++ backend/src/main/java/com/newsletter/cstoday/user/ui/UserController.java
@@ -5,6 +5,7 @@ import com.newsletter.cstoday.user.ui.dto.UserDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +17,7 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping("/register")
+    @CrossOrigin(origins = "https://cs-today.netlify.app/", allowedHeaders = "*")
     public ResponseEntity<Void> register(@Validated @RequestBody UserDto userDto) {
         userService.register(userDto);
         return ResponseEntity.ok().build();
